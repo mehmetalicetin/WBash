022dc2424014fa101155ad8aef5c4bc2f64d86d9
mehmetali.cetin <bm.m.alicetin@gmail.com>
2019-09-19 15:47:01 +0300

    adsada

---
+++ BuilderDesignPattern/src/com/cetin/i2i/builder/base/Main.java
@@ -0,0 +1,19 @@
+package com.cetin.i2i.builder.base;
+
+import com.cetin.i2i.builder.bean.Email;
+
+public class Main {
+
+  public static void main(String[] args) {
+    
+    Email email=new Email.EmailBuilder()
+                         .addRecipient("malicetin@gmail.com")
+                         .setMainText("Hello Builder Desing Patten")
+                         .setGreeting("Hi Ali")
+                         .setClosing("Regards")
+                         .setTitle("Builder Pattern Recources")
+                         .build();
+    
+  }
+
+}
